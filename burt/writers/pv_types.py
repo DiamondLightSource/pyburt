94f9ec4ddfeb6148ee1d2d6f85e47ffe195293a1
Ito Alcuaz <ito.alcuaz@diamond.ac.uk>
2020-01-20 15:49:55 +0000

    write read and write type visitor

--- burt/writers/pv_types.py
+++ burt/writers/pv_types.py
@@ -7,6 +7,12 @@ implemented in the old BURT which are mimicked here.
 This module provides an interface for obtaining and setting channel access pv types
 from cothread. A visitor pattern is implemented to avoid lengthy if/else case statements
 and to take advantage of the new Python 3 single-dispatch generic functions.
+
+The type mapping from a python type to a CA DBR type is as follows:
+
+python int -> DBR_LONG
+python float -> DBR_DOUBLE
+python string -> DBR_STRING
 """
 import logging
 from abc import ABCMeta, abstractmethod
@@ -14,6 +20,12 @@ from functools import singledispatch
 from typing import Any, Dict, List, Tuple
 
 import cothread
+
+# EPICS constraint, max 39 characters in a channel access string.
+MAX_DBR_STR_LEN = 39
+
+# Scalar pv entries are shown as a 15 width precision number(s) in scientific notation.
+SNAP_PRECISION_PYFORMAT = "{:.15e}"
 
 
 class PvVisitor:
@@ -28,8 +40,26 @@ class PvVisitor:
 
     @abstractmethod
     @visit.register(cothread.dbr.ca_str)
-    def visit(self, dbr_val: cothread.dbr.ca_str) -> str:
-        """Visit string type."""
+    def visit(self, dbr_val: cothread.dbr.ca_str) -> Any:
+        """String (DBR_STRING) type."""
+        pass
+
+    @abstractmethod
+    @visit.register(cothread.dbr.ca_int)
+    def visit(self, dbr_val: cothread.dbr.ca_int) -> Any:
+        """Int (DBR_LONG) type."""
+        pass
+
+    @abstractmethod
+    @visit.register(cothread.dbr.ca_float)
+    def visit(self, dbr_val: cothread.dbr.ca_float) -> Any:
+        """Float (DBR_DOUBLE) type."""
+        pass
+
+    @abstractmethod
+    @visit.register(cothread.dbr.ca_array)
+    def visit(self, dbr_val: cothread.dbr.ca_array) -> Any:
+        """CA array type."""
         pass
 
 
@@ -37,43 +67,67 @@ class PvReadVisitor(PvVisitor):
     """Convert and prepare a DBR value for writing to a BURT file."""
 
     @singledispatch
-    def visit(self, dbr_val: object) -> None:
+    def visit(self, dbr_val: object) -> str:
         """Generic case."""
-        return super(PvReadVisitor, self).visit(dbr_val)
+        return SNAP_PRECISION_PYFORMAT.format(dbr_val)
 
     @visit.register(cothread.dbr.ca_str)
     def visit(self, dbr_val: cothread.dbr.ca_str) -> str:
-        """Visit string type."""
+        """String (DBR_STRING) type."""
         return str(dbr_val)
 
-    @visit.register(cothread.dbr.ca_str)
-    def visit(self, dbr_val: cothread.dbr.ca_str) -> int:
-        """Generic visit."""
-        return str(dbr_val)
+    @visit.register(cothread.dbr.ca_int)
+    def visit(self, dbr_val: cothread.dbr.ca_int) -> str:
+        """Int (DBR_LONG) type."""
+        return SNAP_PRECISION_PYFORMAT.format(dbr_val)
+
+    @visit.register(cothread.dbr.ca_float)
+    def visit(self, dbr_val: cothread.dbr.ca_float) -> str:
+        """Float (DBR_DOUBLE) type."""
+        return SNAP_PRECISION_PYFORMAT.format(dbr_val)
+
+    @visit.register(cothread.dbr.ca_array)
+    def visit(self, dbr_val: cothread.dbr.ca_array) -> list:
+        """CA array type."""
+        return [SNAP_PRECISION_PYFORMAT.format(reading) for reading in dbr_val[:]]
 
 
 class PvWriteVisitor(PvVisitor):
     """Convert and prepare a DBR value for writing via channel access."""
 
     @singledispatch
-    def visit(self, dbr_val: object) -> None:
+    def visit(self, dbr_val: object) -> str:
         """Generic case."""
-        return super(PvWriteVisitor, self).visit(dbr_val)
+        return str(dbr_val)
 
     @visit.register(cothread.dbr.ca_str)
     def visit(self, dbr_val: cothread.dbr.ca_str) -> str:
-        """Visit string type."""
+        """String (DBR_STRING) type."""
         return str(dbr_val)
 
-    @visit.register(cothread.dbr.ca_str)
-    def visit(self, dbr_val: cothread.dbr.ca_str) -> int:
-        """Generic visit."""
-        return str(dbr_val)
+    @visit.register(cothread.dbr.ca_int)
+    def visit(self, dbr_val: cothread.dbr.ca_int) -> int:
+        """Int (DBR_LONG) type."""
+        return int(dbr_val)
+
+    @visit.register(cothread.dbr.ca_float)
+    def visit(self, dbr_val: cothread.dbr.ca_float) -> float:
+        """Float (DBR_DOUBLE) type."""
+        return float(dbr_val)
+
+    @visit.register(cothread.dbr.ca_array)
+    def visit(self, dbr_val: cothread.dbr.ca_array) -> list:
+        """CA array type."""
+        return [self.visit(val) for val in dbr_val]
 
 
 class PvTypeDBR:
+    """Encapsulate PV types and convert to DBR format."""
+
     def __init__(self, dbr_val) -> None:
+        """Constructor."""
         self.dbr_val = dbr_val
 
     def accept(self, type_visitor: PvVisitor) -> Any:
+        """Single dispatch on the DBR type to get the desired value."""
         return type_visitor.visit(self.dbr_val)
