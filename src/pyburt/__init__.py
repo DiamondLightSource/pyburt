a9b8b60a30a30b99eb09995a652d4280e6dd9c7d
Phil Smith <pts@observatorysciences.co.uk>
2026-03-13 14:29:47 +0000

    Move pyburt source code into src directory

--- src/pyburt/__init__.py
+++ src/pyburt/__init__.py
@@ -1,11 +1,25 @@
-"""Top level API.
+"""The top-level burt package."""
+from burt._version import __version__  # noqa
+from burt.checks import CheckFailedException, check
+from burt.parsers.check import CheckParser
+from burt.parsers.req import ReqParser
+from burt.parsers.rgr import RgrParser
+from burt.parsers.rqg import RqgParser
+from burt.parsers.snap import SnapParser
+from burt.read import take_snapshot, take_snapshot_group
+from burt.write import restore, restore_group
 
-.. data:: __version__
-    :type: str
-
-    Version number as calculated by https://github.com/pypa/setuptools_scm
-"""
-
-from ._version import __version__
-
-__all__ = ["__version__"]
+__all__ = [
+    "restore",
+    "restore_group",
+    "take_snapshot",
+    "take_snapshot_group",
+    "check",
+    "CheckFailedException",
+    "ReqParser",
+    "SnapParser",
+    "RgrParser",
+    "RqgParser",
+    "CheckParser",
+    "__version__"
+]
